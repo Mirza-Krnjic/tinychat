d1a9c5dd4c46abd9e3aadf76f30ddcbd3d4c3fdf
Krnjic Mirza <krnjicmirza@gmail.com>
2022-05-28 10:43:56 +0200

    login functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../firebase";
-import { setDoc, doc, Timestamp } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
-function Register() {
+function Login() {
   const [data, setData] = useState({
-    name: "",
     email: "",
     password: "",
     error: null,
@@ -15,7 +14,7 @@ function Register() {
 
   const navigate = useNavigate();
 
-  const { name, email, password, loading, error } = data;
+  const { email, password, loading, error } = data;
 
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
@@ -24,24 +23,16 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setData({ ...data, error: null, loading: true });
-    if (!name || !email || !password) {
+    if (!email || !password) {
       setData({ ...data, error: "All fields are required" });
     }
     try {
-      const result = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      await setDoc(doc(db, "users", result.user.uid), {
-        uid: result.user.uid,
-        name,
-        email,
-        createdAt: Timestamp.fromDate(new Date()),
+      const result = await signInWithEmailAndPassword(auth, email, password);
+
+      await updateDoc(doc(db, "users", result.user.uid), {
         isOnline: true,
       });
       setData({
-        name: "",
         email: "",
         password: "",
         error: null,
@@ -55,12 +46,8 @@ function Register() {
 
   return (
     <section>
-      <h3>Create An Account</h3>
+      <h3>Login into your Account</h3>
       <form className="form" onSubmit={handleSubmit}>
-        <div className="input_container">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={handleChange} />
-        </div>
         <div className="input_container">
           <label htmlFor="email">Email</label>
           <input
@@ -82,7 +69,7 @@ function Register() {
         {error ? <p className="error">{error}</p> : null}
         <div className="btn_container">
           <button className="btn" disabled={loading}>
-            {loading ? "Registering..." : "Register"}
+            {loading ? "Logging in ..." : "Login"}
           </button>
         </div>
       </form>
@@ -90,4 +77,4 @@ function Register() {
   );
 }
 
-export default Register;
+export default Login;
